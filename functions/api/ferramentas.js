615c95cf288887b87ae20d34b263b74ff7f49e15
alanfxx <j.alands2@gmail.com>
2020-05-15 21:43:54 -0300

    api peças - referencia unica

--- functions/api/ferramentas.js
+++ functions/api/ferramentas.js
@@ -1,4 +1,5 @@
 module.exports = app => {
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
     const save = async (req, res) => {
         const ferramenta = {
@@ -10,17 +11,24 @@ module.exports = app => {
             existsOrError(ferramenta.name, 'Nome não informado')
             existsOrError(ferramenta.quant, 'Quantidade não informada')
 
+            let ferramentaFromDB
+            await app.config.db.ferramentas.get().then(docs => {
+                docs.forEach(doc => {
+                    if (doc.data().name === ferramenta.name) {
+                        ferramentaFromDB = {
+                            id: doc.id,
+                            name: doc.data().name,
+                            quant: doc.data().quant
+                        }
+                    }
+                })
+            })
             if (!req.params.id) {
-                let ferramentaFromDB
-                await app.config.db.ferramentas.get()
-                    .then(docs => {
-                        docs.forEach(doc => {
-                            if (doc.data().name === ferramenta.name) {
-                                ferramentaFromDB = doc.data()
-                            }
-                        })
-                    })
-                notExistsOrError(ferramentaFromDB, 'Ferramenta já cadastrada')
+                notExistsOrError(ferramentaFromDB, 'Já existe ferramenta com esse nome')
+            } else {
+                if(ferramentaFromDB){
+                    equalsOrError(ferramentaFromDB.id, req.params.id,'Já existe ferramenta com esse nome')
+                }
             }
         } catch (mssg) {
             return res.status(400).send(mssg)
@@ -28,37 +36,35 @@ module.exports = app => {
         // if (req.params.id) ferramenta.id = req.params.id
         if (req.params.id) {
             let documentRef = app.config.db.ferramentas.doc(req.params.id)
-            await documentRef.update(ferramenta)
+            return await documentRef.update(ferramenta)
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
-            await app.config.db.ferramentas.add(ferramenta)
+            return await app.config.db.ferramentas.add(ferramenta)
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         }
     }
 
     const get = async (req, res) => {
-        app.config.db.ferramentas.get()
-            .then(docs => {
-                let ferramentas = []
-                docs.forEach(doc => {
-                    ferramentas.push({
-                        id: doc.id,
-                        name: doc.data().name,
-                        quant: doc.data().quant
-                    })
+        return await app.config.db.ferramentas.get().then(docs => {
+            let ferramentas = []
+            docs.forEach(doc => {
+                ferramentas.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    quant: doc.data().quant
                 })
-                res.status(200).json(ferramentas)
             })
-            .catch(err => res.status(500).send(err))
+            res.status(200).json(ferramentas)
+        }).catch(err => res.status(500).send(err))
     }
 
     const remove = async (req, res) => {
         let documentRef = app.config.db.ferramentas.doc(req.params.id)
-        documentRef.delete().then(() => {
-            res.status(204).send()
-        }).catch(err => res.status(500).send(err))
+        return await documentRef.delete()
+            .then(() => res.status(204).send())
+            .catch(err => res.status(500).send(err))
     }
 
     return { save, get, remove }
--- functions/api/pecas.js
+++ functions/api/pecas.js
@@ -1,4 +1,5 @@
 module.exports = app => {
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
     const save = async (req, res) => {
         const peca = {
@@ -12,17 +13,25 @@ module.exports = app => {
             existsOrError(peca.ref, 'Referência não informada')
             existsOrError(peca.quant, 'Quantidade não informada')
 
+            let pecaFromDB
+            await app.config.db.pecas.get().then(docs => {
+                docs.forEach(doc => {
+                    if (doc.data().ref === peca.ref) {
+                        pecaFromDB = {
+                            id: doc.id,
+                            name: doc.data().name,
+                            ref: doc.data().ref,
+                            quant: doc.data().quant
+                        }
+                    }
+                })
+            })
             if (!req.params.id) {
-                let pecaFromDB
-                await app.config.db.pecas.get()
-                    .then(docs => {
-                        docs.forEach(doc => {
-                            if (doc.data().name === peca.name) {
-                                pecaFromDB = doc.data()
-                            }
-                        })
-                    })
-                notExistsOrError(pecaFromDB, 'Peça já cadastrada')
+                notExistsOrError(pecaFromDB, 'Já existe peça com essa referência')
+            } else {
+                if(pecaFromDB){
+                    equalsOrError(pecaFromDB.id, req.params.id,'Já existe peça com essa referência')
+                }
             }
         } catch (mssg) {
             return res.status(400).send(mssg)
@@ -30,38 +39,36 @@ module.exports = app => {
         // if (req.params.id) peca.id = req.params.id
         if (req.params.id) {
             let documentRef = app.config.db.pecas.doc(req.params.id)
-            await documentRef.update(peca)
+            return await documentRef.update(peca)
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
-            await app.config.db.pecas.add(peca)
+            return await app.config.db.pecas.add(peca)
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         }
     }
 
     const get = async (req, res) => {
-        app.config.db.pecas.get()
-            .then(docs => {
-                let pecas = []
-                docs.forEach(doc => {
-                    pecas.push({
-                        id: doc.id,
-                        name: doc.data().name,
-                        ref: doc.data().ref,
-                        quant: doc.data().quant
-                    })
+        return await app.config.db.pecas.get().then(docs => {
+            let pecas = []
+            docs.forEach(doc => {
+                pecas.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    ref: doc.data().ref,
+                    quant: doc.data().quant
                 })
-                res.status(200).json(pecas)
             })
-            .catch(err => res.status(500).send(err))
+            res.status(200).json(pecas)
+        }).catch(err => res.status(500).send(err))
     }
 
     const remove = async (req, res) => {
         let documentRef = app.config.db.pecas.doc(req.params.id)
-        documentRef.delete().then(() => {
-            res.status(204).send()
-        }).catch(err => res.status(500).send(err))
+        return await documentRef.delete()
+            .then(() => res.status(204).send())
+            .catch(err => res.status(500).send(err))
     }
 
     return { save, get, remove }
